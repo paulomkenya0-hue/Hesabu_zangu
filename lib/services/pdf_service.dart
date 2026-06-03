ed5631afaf1acfa8c53e71313cdeb5bcdb9cee61
paulomkenya0-hue <paulomkenya0@gmail.com>
2026-06-03 18:28:39 +0300

    Update pdf_service.dart

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -5,7 +5,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:open_file/open_file.dart';
-import '../models/transaction_model.dart';
 import '../services/hive_service.dart';
 
 class PdfService {
